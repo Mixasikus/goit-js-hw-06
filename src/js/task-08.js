63ff1a68daadc4703a0042d637afc1482948b1b4
Mixasikus <mixasiikus@gmail.com>
2022-05-29 11:48:04 +0300

    закончил все задания

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,24 +1,25 @@
 const form = document.querySelector(".login-form");
-const emailEl = document.querySelector('input[type="email"]').value;
-const passwordEl = document.querySelector('input[type="password"]').value;
 
-let formElements = {
-    email: "", password: "" };
-    
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
     console.log(formData);
 
-    if (emailEl || passwordEl === "") {
-        alert("Все поля должны быть заполнены!");
-    } else  {
-        formElements.email = emailEl;
-        formElements.password = passwordEl;
-    }
+    formData.forEach((value, name) => {
+        console.log(name);
+        console.log(value);   
+    });
     
-document.querySelector('.login-form').reset();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        alert("Все поля должны быть заполнены!");
+    }
+
+    form.reset();
 });
 
 
